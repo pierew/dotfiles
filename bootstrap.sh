f2bb59648648e1a4d21e6a0249ce9e4ea6310804
Piere Woehl <s42@uber.space>
2018-12-10 20:23:35 +0100

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for item in $(ls -A -1 ~/.dotfiles/ | grep -e "^\.")
+for item in $(ls -d -A -1 ~/.dotfiles/ | grep -e "^\.")
+do
+    ln -s ~/.dotfiles/${item}
+done
+
+for item in $(ls -f -A -1 ~/.dotfiles/ | grep -e "^\.")
 do
     ln -s ~/.dotfiles/${item}
 done
